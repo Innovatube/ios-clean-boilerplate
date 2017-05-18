ee4725aaf94477a3ea948675b7a639702798ed74
Do Dinh Thy  Son <dodinhthyson@gmail.com>
2017-05-18 16:49:47 +0700

    #6 Finish HTTP and JWT AuthProvider AuthProvider is now a superclass ( rather than protocol )

--- CleanBoilerplate/AuthProvider.swift
+++ CleanBoilerplate/AuthProvider.swift
@@ -14,16 +14,85 @@ struct AuthError : Swift.Error {
   let message : String
 }
 
-protocol AuthProvider {
-  var loginTarget : APITargetType { get }
-  var logoutTarget : APITargetType { get }
-  var authState : AuthState { get set }
-  
-  func login()
-  func logout()
-  func request(_ target: APITargetType) -> Observable<Response>
-}
-
 extension AuthProvider {
   static var queueID : String { return "com.tyson.AuthQueue" }
+}
+
+class AuthProvider {
+  //Should be override by subclass
+  var provider : APIProvider { return APIProvider() }
+  var authState : AuthState = .initial
+  var queue: DispatchQueue = DispatchQueue(label: queueID, attributes: [])
+  let loginTarget : APITargetType
+  let logoutTarget : APITargetType
+  
+  init(loginTarget: APITargetType, logoutTarget: APITargetType){
+    self.loginTarget = loginTarget
+    self.logoutTarget = logoutTarget
+  }
+  
+  fileprivate func advanceAuthState(_ nextState: AuthState) {
+    func login() -> Observable<()> {
+      return provider.request(MultiTarget(loginTarget)).retry(3).filterSuccessfulStatusCodes().map{ _ in () }
+    }
+    
+    func logout() -> Observable<()> {
+      return provider.request(MultiTarget(logoutTarget)).retry(3).filterSuccessfulStatusCodes().map{ _ in () }
+    }
+    
+    func execute(){
+      switch authState {
+      case .initial:
+        if nextState == .noToken {
+          authState = nextState
+        } else {
+          print("Invalid nextState")
+        }
+      case .noToken:
+        if nextState == .authenticated {
+          login().subscribe(onNext: { _ in
+            self.authState = nextState
+          }, onError: { (_) in
+            self.authState = .noToken
+            print("Login unsuccessfully")
+          }).addDisposableTo(DisposeBag())
+        } else {
+          print("Invalid nextState")
+        }
+      case .authenticated:
+        if nextState == .noToken {
+          logout().subscribe(onNext: { _ in
+            self.authState = nextState
+          }, onError: { (_) in
+            self.authState = .authenticated
+            print("Login unsuccessfully")
+          }).addDisposableTo(DisposeBag())
+        } else {
+          print("Invalid nextState")
+        }
+      }
+    }
+    queue.async{ execute() }
+  }
+  
+  open func login() {
+    if authState.canTransitionTo(.authenticated){
+      advanceAuthState(.authenticated)
+    }
+  }
+  
+  open func request(_ target: APITargetType) -> Observable<Response> {
+    if authState.support(target: target) {
+      return provider.request(MultiTarget(target))
+    } else {
+      return Observable.error(AuthError(message:"Couldn't request \(target) due to invalid authState"))
+    }
+  }
+  
+  func logout() {
+    if authState.canTransitionTo(.noToken) {
+      advanceAuthState(.noToken)
+    }
+  }
+
 }
--- CleanBoilerplate/HTTPAuthProvider.swift
+++ CleanBoilerplate/HTTPAuthProvider.swift
@@ -12,90 +12,29 @@ import Alamofire
 import RxSwift
 
 class HTTPAuthProvider : AuthProvider {
-  lazy var provider : APIProvider = APIProvider(plugins: [CredentialsPlugin { _ in
-    URLCredential(user: self.credential?.username ?? "",
-                  password: self.credential?.password ?? "",
-                  persistence: .none) }
-    ] )
   
   public private(set) var credential : Credential?
-  
-  var authState : AuthState = .initial
-  var queue: DispatchQueue = DispatchQueue(label: queueID, attributes: [])
-  let loginTarget : APITargetType
-  let logoutTarget : APITargetType
-  
-  init(loginTarget: APITargetType, logoutTarget: APITargetType){
-    self.loginTarget = loginTarget
-    self.logoutTarget = logoutTarget
+  override var provider : APIProvider {
+    if _provider == nil {
+      _provider = providerFactory()
+    }
+    guard let _provider = _provider else { fatalError("Missing provider object") }
+    return _provider
   }
+  private var _provider : APIProvider?
+  private var token : AuthToken?
   
-  fileprivate func advanceAuthState(_ nextState: AuthState) {
-    func login() -> Observable<()> {
-      return provider.request(MultiTarget(loginTarget)).retry(3).filterSuccessfulStatusCodes().map{ _ in () }
-    }
-    
-    func logout() -> Observable<()> {
-      return provider.request(MultiTarget(logoutTarget)).retry(3).filterSuccessfulStatusCodes().map{ _ in () }
-    }
-    
-    func execute(){
-      switch authState {
-      case .initial:
-        if nextState == .noToken {
-          authState = nextState
-        } else {
-          print("Invalid nextState")
-        }
-      case .noToken:
-        if nextState == .authenticated {
-          login().subscribe(onNext: { _ in
-            self.authState = nextState
-          }, onError: { (_) in
-            self.authState = .noToken
-            print("Login unsuccessfully")
-          }).addDisposableTo(DisposeBag())
-        } else {
-          print("Invalid nextState")
-        }
-      case .authenticated:
-        if nextState == .noToken {
-          logout().subscribe(onNext: { _ in
-            self.authState = .noToken
-          }, onError: { (_) in
-            self.authState = .authenticated
-            print("Logout unsuccessfully")
-          }).addDisposableTo(DisposeBag())
-        } else {
-          print("Invalid nextState")
-        }
-      }
-    }
-    queue.async{ execute() }
+  fileprivate func providerFactory() -> APIProvider {
+    guard let credential = credential else { return APIProvider() }
+    return APIProvider(plugins: [CredentialsPlugin { _ in
+      URLCredential(user: credential.username,
+                    password: credential.password,
+                    persistence: .none) }
+      ] )
   }
   
   // Function for setting credential in case of require credential input validation
   open func set(credential: Credential){
     self.credential = credential
-  }
-  
-  open func login() {
-    if authState.canTransitionTo(.authenticated){
-      advanceAuthState(.authenticated)
-    }
-  }
-  
-  open func request(_ target: APITargetType) -> Observable<Response> {
-    if authState.support(target: target) {
-      return provider.request(MultiTarget(target))
-    } else {
-      return Observable.error(AuthError(message:"Couldn't request \(target) due to invalid authState"))
-    }
-  }
-  
-  func logout() {
-    if authState.canTransitionTo(.noToken) {
-      advanceAuthState(.noToken)
-    }
   }
 }
